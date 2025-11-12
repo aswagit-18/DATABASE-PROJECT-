91ec5b1912283c01be9cd570e5162f241997e9bd
aswagit-18 <aswathiru@gmail.com>
2025-11-12 13:38:01 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -2,15 +2,16 @@ import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 import complaintsRoutes from "./routes/complaints.js";
-import reportRoutes from "./reports/reportRoutes.js"; 
+import reportRoutes from "./reports/reportRoutes.js";
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
+app.use(express.json()); // 
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/complaints", complaintsRoutes);
-app.use("/api/reports", reportRoutes);         
+app.use("/api/reports", reportRoutes);
 app.get("/", (req, res) => {
-  res.send("Server is running!");
+  res.send("Server is running and connected!");
 });
 const PORT = 5050;
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`🚀 Server running at http://localhost:${PORT}`));
